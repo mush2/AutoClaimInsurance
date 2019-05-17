28ab8872b1bd3dd984d8d42eb59a407de5419727
mush2 <mustafas78684@gmail.com>
2019-05-17 11:37:02 +0530

    first Commit

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DatabaseConnectivity.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+                <br />
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                <br />
+                <asp:Button ID="Button1" runat="server" Text="Button"></asp:Button>
+            <table>
+                <tr rowspan="4"><td><asp:GridView ID="GridView1" runat="server"></asp:GridView></td></tr>
+                <tr><td></td></tr>
+                <tr><td></td></tr>
+                <tr>
+                    <td></td></tr>
+            </table>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+                </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectivity
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con1 = new SqlConnection();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con1.ConnectionString = "Data Source=CGI-JAVA-116\\CGI;Initial Catalog = abu;Integrated Security = true"; ;
+            con1.Open();
+            SqlCommand cmd = new SqlCommand("select & from employees where empId=@empID", con1);
+            SqlParameter param1 = cmd.Parameters.Add("@empId", SqlDbType.VarChar, 15);
+            param1.Value = TextBox1.Text;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Employees");
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+    }
+}
